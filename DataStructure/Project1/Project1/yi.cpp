9ab8775ec8c4268127c4b74c024954324e53dd46
Linggg <lingyu4113@gmail.com>
2019-05-27 23:15:11 +0800

    1

---
+++ DataStructure/Project1/Project1/yi.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#define ElementType int
+#define MAXSIZE 1000
+//线性表顺序存储实现
+
+typedef struct LNode *List;
+struct LNode {
+	ElementType Data[MAXSIZE];
+	int Last;
+
+};
+
+struct LNode L;
+List Ptrl;
+//访问下标为i的元素：L.Data[i]或 Ptrl->Data[i]
+//线性表的长度：L.last+1或Ptrl->Last+1
+
+
+//初始化
+List MakeEmpty()
+{
+	List Ptrl;
+	Ptrl = (List) malloc (sizeof(struct LNode));
+	Ptrl->Last = -1;
+	return Ptrl;
+
+}
+
+//查找,平均比较次数为(n+1)/2，平均时间性能为O(n)
+int Find(ElementType X, List  Ptrl)
+{
+	int i = 0;
+	while (i <= Ptrl->Last && Ptrl->Data[i] != X)
+		i++;
+	if (i > Ptrl->Last)
+		return  -1;//没找到返回-1
+	else return i;//找到返回的是存储位置
+
+}
+
+//插入操作
+void Insert(ElementType X, int i, List Ptrl)
+{
+	int i, j;
+	if (Ptrl->Last == MAXSIZE - 1) 
+	{
+		printf("man");
+		return;
+	}
+	if (i < 1 || Ptrl->Last + 2) 
+	{
+		printf("weizhi bu he fa");
+		return;
+	}
+	for (j = Ptrl->Last; j >= i - 1; j--) 
+	{
+		Ptrl->Data[j + 1] = Ptrl->Data[j];//将ai到an倒序向后移动
+	}
+	Ptrl->Data[i - 1] = X;//新元素插入
+	Ptrl->Last++;//Last仍指向最后元素
+	return;
+}
